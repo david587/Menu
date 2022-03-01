2301cd37b4cc4c7f6965ff38e16e65b3c0143cf2
Baráth Dávid <davidbarath08@gmail.com>
2022-03-01 09:56:08 +0100

    Kész

--- src/Menu.java
+++ src/Menu.java
@@ -2,19 +2,61 @@ import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new.Scanner(System.in);
-        System.out.println("-------------------------");
-        System.out.println();
-        System.out.println("1) Program inditása");
-        System.out.println();
-        System.out.println("2) Nevjegy");
-        System.out.println();
-        System.out.println("3) Kilepes");
+        Scanner sc = new Scanner(System.in);
+        String valasztott="";
 
-        System.out.println();
-        System.out.println();
-        System.out.println("Valasztas");
-        String valasztott =sc.nextLine();
+        do{
+            System.out.println("-------------------------");
+            System.out.println();
+            System.out.println("1) Program inditása");
+           
+            System.out.println("2) Névjegy");
+            
+            System.out.println("3) Kilépes");
+    
+            
+            System.out.println();
+            System.out.print("Választás:");
+            valasztott =sc.nextLine();
+            switch(valasztott){
+                case "1":
+                System.out.print("Program...");
+
+                double szam=0;
+                do{
+                    System.out.print("szam");
+                    szam = sc.nextDouble();
+                    if(szam !=0){
+                        double ngyok=Math.sqrt(szam);
+                        System.out.printf("Negyzetgyök: %.2f\n",ngyok
+                        );
+                    }
+                }while(szam!=0);
+                System.out.println("Folytatashoz enter");
+                sc.nextLine();//Enter olvasása
+                sc.nextLine();//várás az enterre
+
+                break;
+    
+                case "2":
+                System.out.print("Nevjegy...");
+                System.out.println("Baráth Dávid");
+                System.out.println("Szoft1/N");
+                System.out.println("gyökszamitó");
+                System.out.println("folytatáshoz enter");
+                sc.nextLine();
+                break;
+    
+                case "3":
+                System.out.println("Kilepes...");
+                break;
+            }
+        } 
+
+while(!valasztott.equals("3"));
+
+       
+      
     }
 }
 
